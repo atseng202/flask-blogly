c986caec2d9690570873f90de849000f8acd1f7e
Alan Tseng <atseng202@gmail.com>
2020-12-09 17:13:31 -0800

    Add full name convenience method for users

--- models.py
+++ models.py
@@ -34,4 +34,8 @@ class User(db.Model):
         new_user = User(first_name="Test", last_name="Name")
         db.session.add(new_user)
         db.session.commit()
-        
+    
+    def get_full_name(self):
+        """ Return the user's full name """
+
+        return f"{self.first_name} {self.last_name}"
